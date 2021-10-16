5d26bd1fa6b5ac75bbf32c4190101dd68f8c30a6
m0-ar <s176492@student.dtu.dk>
2021-10-16 13:20:53 +0200

    Detect the button that the user clicked on

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,20 @@ var buttonColours = ["red", "blue", "green", "yellow"]
 
 var gamePattern = []
 
+var userClickedPattern = []
+
+// Detect the button that the user clicked on
+$('.btn').on('click',function(){
+    console.log("button got clicked")
+
+    var userChosenColour = $(this).attr("id")
+    console.log(userChosenColour)
+
+    userClickedPattern.push(userChosenColour)
+    console.log(userClickedPattern)
+}); 
+
+// Flash and display sound of the button
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 
